352613c61b2e19266b64b507a2a4d9daefa5e39e
zhaoyu <zhaoyu@gram-tech.com>
2023-07-31 23:39:43 +0800

    fix:三叔之和

---
+++ arrayTopic/threeSum.java
@@ -0,0 +1,18 @@
+import java.util.List;
+
+/**
+ * 输入: [-1,0,1,2,-1,-4]
+ * 两个三元组的和为0，分别是[-1,0,1]和[-1,-1,2]
+ */
+
+public class threeSum {
+    public static void main(String[] args) {
+
+    }
+
+    public List<List<Integer>> threeSum(int[] nums) {}
+
+    private void twoSum(int[] nums, int i, List<List<Integer>> result) {
+
+    }
+}
